0e72ca5e3548027d876a950f774df4382146629d
skatpgusskat <skatpgusskat@naver.com>
2018-07-07 16:47:16 +0000

    Done!

--- public/index.js
+++ public/index.js
@@ -20,5 +20,31 @@ events.forEach(event => {
   socket.emit('sub', event);
   socket.on(event, (data) => {
     console.log(event, data);
+    const {
+      elapsedTime,
+      distance,
+      strokeRate,
+      currentPace,
+    } = data;
+
+    if (elapsedTime) {
+      const minute = Math.floor((elapsedTime / 1000) / 60);
+      const second = Math.floor((elapsedTime / 1000) % 60);
+      document.getElementById('elapsed-time-value').innerHTML = `${minute === 0 ? '' : minute}:${second.toString().padStart(2, '0')}`;
+    }
+
+    if (distance) {
+      document.getElementById('distance-value').innerHTML = `${Math.floor(distance)}`;
+    }
+
+    if (strokeRate) {
+      document.getElementById('stroke-rate-value').innerHTML = `${strokeRate}`;
+    }
+
+    if (currentPace) {
+      const minute = Math.floor((currentPace / 1000) / 60);
+      const second = Math.floor((currentPace / 1000) % 60);
+      document.getElementById('current-pace-value').innerHTML = `${minute === 0 ? '' : minute}:${second.toString().padStart(2, '0')}`;
+    }
   });
 });
